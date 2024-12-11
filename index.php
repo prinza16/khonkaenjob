4d5ed83c5f9e31579bceb526fa1c563c1dd2e949
unknown <pattyawat191@gmail.com>
2024-12-11 11:02:10 +0700

    update

--- index.php
+++ index.php
@@ -1,8 +1,51 @@
+<?php
+include('rss_url_connect.php');
+?>
 <?php include('h.php') ?>
 <?php include('navbar.php') ?>
 
 <?php if (isset($_SESSION['username'])) : ?>
-    user
+    <div class="d-flex py-4">
+        <div class="col-lg-2">
+            <p></p>
+        </div>
+        <div class="card col-lg-8 border-0">
+            <div class="card-body rounded" style="border: 1px solid #E0E0E0;box-shadow: 0px 15px 15px rgba(224, 224, 224, 1);">
+                <form class="d-block" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-custom-search container-fluid mt-3 fw-semibold" type="button">Search</button>
+                    <div class="mt-3">
+                        <?php
+                        foreach ($rss->job as $job) {
+                            if (stripos($job->region, 'Chonburi') !== false) {
+                                echo "
+                                    <a href='" . htmlspecialchars($job->link) . "' target='_blank' class='d-flex bg-white rounded p-2 mt-2 cursor-pointer custom-class-card-hightlight' 
+                                    style='text-decoration: none; color: inherit;border: 1px solid #E0E0E0;'>
+                                        <div class='col-6 ps-2'>
+                                            <label class='fs-4 fw-normal d-block' style='cursor: pointer;' style='color:#333333;'>" . htmlspecialchars($job->name) . "</label>
+                                            <label class='fs-6 fw-semibold d-block cursor-pointer' style='color:#b3bebe;cursor: pointer;'>" . htmlspecialchars($job->company) . "</label>
+                                        </div>
+                                        <div class='col-3'>
+                                            <label class='fs-6 fw-medium' style='color:#9E9E9E;cursor: pointer;'>" . htmlspecialchars($job->region) . "</label>
+                                        </div>
+                                        <div class='col-3 text-end pe-2 d-flex flex-column justify-content-center'>
+                                            <label class='fs-6 fw-semibold d-block' style='color:#9E9E9E;cursor: pointer;'>" . htmlspecialchars($job->jobtype) . "</label>
+                                            <label class='d-block' style='cursor: pointer;'>" . htmlspecialchars($job->updated) . "</label>
+                                        </div>
+
+                                    </a>
+                                ";
+                            }
+                        }
+                        ?>
+                    </div>
+
+
+                </form>
+            </div>
+        </div>
+        <div class="col-lg-2"> </div>
+    </div>
 <?php endif ?>
 
 <?php include('footer.php') ?>
--- navbar.php
+++ navbar.php
@@ -10,36 +10,39 @@ include('h.php');
         <div class="collapse navbar-collapse justify-content-between" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item btn-custom-navbar-menu">
-                    <a class="nav-link active text-custom-navbar-menu fs-6 fw-bold" style="color: #64748b;" aria-current="page" href="index.php">Home</a>
+                    <?php
+                    $activeClass = (basename($_SERVER['PHP_SELF']) == 'index.php') ? 'active' : '';
+                    ?>
+                    <a class="nav-link <?php echo $activeClass; ?> text-custom-navbar-menu fs-6 fw-bold" style="color: #64748b;" aria-current="page" href="index.php">Home</a>
                 </li>
             </ul>
 
             <ul class="navbar-nav">
                 <?php
-                    if (isset($_SESSION['username'])) {
-                        echo '<li class="nav-item">
+                if (isset($_SESSION['username'])) {
+                    echo '<li class="nav-item">
                                 <a class="nav-link text-black fw-semibold" href="profile.php">Welcome, ' . $_SESSION['fullname'] . '</a>
                               </li>';
-                        // if (isset($_SESSION['profile_picture'])) {
-                        //     echo '<li class="nav-item">
-                        //             <img src="' . $_SESSION['profile_picture'] . '" alt="Profile Picture" style="width: 30px; height: 30px; border-radius: 50%; margin-right: 10px;">
-                        //           </li>';
-                        // }
-                        echo '<li class="nav-item">
+                    // if (isset($_SESSION['profile_picture'])) {
+                    //     echo '<li class="nav-item">
+                    //             <img src="' . $_SESSION['profile_picture'] . '" alt="Profile Picture" style="width: 30px; height: 30px; border-radius: 50%; margin-right: 10px;">
+                    //           </li>';
+                    // }
+                    echo '<li class="nav-item">
                                 <a class="nav-link text-white btn-custom-logout-navbar fw-semibold" href="index.php?logout=true">Logout</a>
                               </li>';
-                    } else {
-                        echo '
+                } else {
+                    echo '
                             <li class="nav-item">
                                 <a class="nav-link btn-custom-login-navbar fs-6 fw-bold me-3 px-3" href="login.php">Login</a>
                             </li>
                         ';
-                        echo '
+                    echo '
                             <li class="nav-item">
                                 <a class="nav-link btn-custom-register-navbar fs-6 fw-bold px-3" href="register.php">Register</a>
                             </li>
                         ';
-                    }
+                }
                 ?>
             </ul>
         </div>
