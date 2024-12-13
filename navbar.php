c3ea157076d4b5cdcae53ce6c978b8f3a5e9fb88
unknown <pattyawat191@gmail.com>
2024-12-13 17:15:53 +0700

    update

--- navbar.php
+++ navbar.php
@@ -3,7 +3,7 @@ include('h.php');
 ?>
 <nav class="navbar navbar-expand-lg px-5" style="box-shadow: 0 .125rem .25rem rgba(2,6,23,.075);padding: .5rem;background: #ffffff;">
     <div class="container-fluid">
-        <a class="navbar-brand text-black" href="#">Navbar</a>
+        <a class="navbar-brand text-black" href="#"><label>Navbar</label></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -13,7 +13,7 @@ include('h.php');
                     <?php
                     $activeClass = (basename($_SERVER['PHP_SELF']) == 'index.php') ? 'active' : '';
                     ?>
-                    <a class="nav-link <?php echo $activeClass; ?> text-custom-navbar-menu fs-6 fw-bold" style="color: #64748b;" aria-current="page" href="index.php">Home</a>
+                    <a class="nav-link <?php echo $activeClass; ?> text-custom-navbar-menu fs-6 fw-bold" style="color: #64748b;" aria-current="page" href="index.php"><label>Home</label></a>
                 </li>
             </ul>
 
@@ -22,7 +22,7 @@ include('h.php');
                 if (isset($_SESSION['username'])) {
                     echo '<li class="nav-item d-flex align-items-center p-1 btn btn-light me-2">
                               <img src="https://plus.unsplash.com/premium_photo-1664541336692-e931d407ba88?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Profile Picture" style="width: 30px; height: 30px; border-radius: 50%; margin-right: 10px;">
-                              <a class="nav-link text-black fw-semibold" href="profile.php">Profile</a>
+                              <a class="nav-link fw-semibold" href="profile.php" style="color:#64748b">Profile</a>
                             </li>
                             <li class="nav-item btn btn-light">
                                 <a class="nav-link fw-semibold" href="index.php?logout=true"><span>Logout</span></a>   
